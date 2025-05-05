022a5d4728d4b66523534e77fefea8162104d11b
yoshipon-tech <yoshipon.tech@gmail.com>
2025-05-06 00:09:29 +0900

    条件分岐done

--- src/cLevel/conditional/main.js
+++ src/cLevel/conditional/main.js
@@ -38,3 +38,69 @@ function zeroF() {
         var answerSome = lines.slice(1, number + 1).some(function (line) { return line === "0"; });
     });
 }
+function better() {
+    process.stdin.resume();
+    process.stdin.setEncoding("utf8");
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    var lines = [];
+    var reader = require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    reader.on("line", function (line) {
+        lines.push(line);
+    });
+    reader.on("close", function () {
+        var _a = lines[0].split(" ").map(Number), left = _a[0], middle = _a[1], right = _a[2];
+        console.log((left > 9 && middle > 9) || right > 9 ? "YES" : "NO");
+    });
+}
+function fizzBuzz() {
+    process.stdin.resume();
+    process.stdin.setEncoding("utf8");
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    var lines = [];
+    var reader = require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    reader.on("line", function (line) {
+        lines.push(line);
+    });
+    reader.on("close", function () {
+        var number = Number(lines[0]);
+        var fizzBuzz = function (n) {
+            return n % 3 === 0 && n % 5 === 0
+                ? "FizzBuzz"
+                : n % 3 === 0
+                    ? "Fizz"
+                    : n % 5 === 0
+                        ? "Buzz"
+                        : n;
+        };
+        console.log(fizzBuzz(number));
+    });
+}
+function final() {
+    process.stdin.resume();
+    process.stdin.setEncoding("utf8");
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    var lines = [];
+    var reader = require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    reader.on("line", function (line) {
+        lines.push(line);
+    });
+    reader.on("close", function () {
+        var _a = lines[0].split(" ").map(Number), left = _a[0], right = _a[1];
+        var calc = function (x, y) {
+            return x > 0 ? x * x : y > 0 ? x * y : y * y;
+        };
+        console.log(calc(left, right));
+    });
+}
--- src/cLevel/conditional/main.ts
+++ src/cLevel/conditional/main.ts
@@ -44,3 +44,76 @@ function zeroF() {
     const answerSome = lines.slice(1, number + 1).some((line) => line === "0");
   });
 }
+
+function better() {
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+  // 自分の得意な言語で
+  // Let's チャレンジ！！
+  const lines: string[] = [];
+  const reader = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  reader.on("line", (line) => {
+    lines.push(line);
+  });
+  reader.on("close", () => {
+    const [left, middle, right] = lines[0].split(" ").map(Number);
+
+    console.log((left > 9 && middle > 9) || right > 9 ? "YES" : "NO");
+  });
+}
+
+function fizzBuzz() {
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+  // 自分の得意な言語で
+  // Let's チャレンジ！！
+  const lines: string[] = [];
+  const reader = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  reader.on("line", (line) => {
+    lines.push(line);
+  });
+  reader.on("close", () => {
+    const number = Number(lines[0]);
+
+    const fizzBuzz = (n) => {
+      return n % 3 === 0 && n % 5 === 0
+        ? "FizzBuzz"
+        : n % 3 === 0
+        ? "Fizz"
+        : n % 5 === 0
+        ? "Buzz"
+        : n;
+    };
+
+    console.log(fizzBuzz(number));
+  });
+}
+
+function final() {
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+  // 自分の得意な言語で
+  // Let's チャレンジ！！
+  const lines: string[] = [];
+  const reader = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  reader.on("line", (line) => {
+    lines.push(line);
+  });
+  reader.on("close", () => {
+    const [left, right] = lines[0].split(" ").map(Number);
+
+    const calc = (x, y) => {
+      return x > 0 ? x * x : y > 0 ? x * y : y * y;
+    };
+    console.log(calc(left, right));
+  });
+}
